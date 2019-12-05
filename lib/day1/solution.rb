021a8ad2257a9981d2a08cce16dddd8c1c44784e
Luuk Veenis <luuk@juul.com>
2019-12-04 20:42:08 -0800

    Add recursive fuel required for part 2
    Part 1 of the day 1 puzzle asked you to compute the fuel required for
    the spacecraft to launch into space using the provided calculation.
    
    For part 2, it was clearly asking for a recursive version of the
    calculation where the result of the first time it's performed gets fed
    into the next time, and so on until the input is <= 0.

--- lib/day1/solution.rb
+++ lib/day1/solution.rb
@@ -2,5 +2,13 @@ def fuel_required(mass)
   mass / 3 - 2
 end
 
+def recursive_fuel_required(mass)
+  fuel_required = mass / 3 - 2
+  return 0 if fuel_required <= 0
+
+  fuel_required + recursive_fuel_required(fuel_required)
+end
+
 input = File.readlines(ARGV[0], chomp: true)
-puts input.sum { |line| fuel_required(line.to_i) }
+puts "Part 1: #{input.sum { |line| fuel_required(line.to_i) } }"
+puts "Part 2: #{input.sum { |line| recursive_fuel_required(line.to_i) } }"
