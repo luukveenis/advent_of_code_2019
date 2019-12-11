67a09b33968c344a620529ea205ea8a0abd01966
Luuk Veenis <luuk@juul.com>
2019-12-10 20:22:08 -0800

    Turn computer into a classTurn computer into a class

--- lib/day2/solution.rb
+++ lib/day2/solution.rb
@@ -1,22 +1,38 @@
-def intcode(input)
-  values = input.split(",").map!(&:to_i)
+class Computer
+  attr_accessor :memory
 
-  0.step(by: 4, to: values.length - 1) do |index|
-    case values[index]
-    when 1
-      values[values[index + 3]] = values[values[index + 1]] + values[values[index + 2]]
-    when 2
-      values[values[index + 3]] = values[values[index + 1]] * values[values[index + 2]]
-    when 99
-      return values.join(",")
-    else
-      raise "Invalid opcode"
+  def initialize(intcode)
+    @memory = intcode.split(",").map!(&:to_i)
+  end
+
+  def call
+    0.step(by: 4, to: memory.length - 1) do |pointer|
+      case memory_at(pointer)
+      when 1
+        memory[memory_at(pointer + 3)] = value_at(pointer + 1) + value_at(pointer + 2)
+      when 2
+        memory[memory_at(pointer + 3)] = value_at(pointer + 1) * value_at(pointer + 2)
+      when 99
+        return memory.join(",")
+      else
+        raise "Invalid opcode"
+      end
     end
+  end
+
+  private
+
+  def memory_at(pointer)
+    memory[pointer]
+  end
+
+  def value_at(pointer)
+    memory[memory[pointer]]
   end
 end
 
 input = File.readlines(ARGV[0], chomp: true)
 
 input.each_with_index do |line, index|
-  puts intcode(line)
+  puts Computer.new(line).call
 end
